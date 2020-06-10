0112b482155dd011bad13bf68fa0c35f08bbff08
Salvador Alexis Virgen Flores <salexisvirgen@gmail.com>
2020-06-10 05:15:57 -0500

    added hardcode points to test animator

--- core_lib/animator.py
+++ core_lib/animator.py
@@ -11,15 +11,15 @@ from core import get_config_value
 
 # this two variables will be provided by some other module
 points = [(8,233),(12,233),(16,233),(20,233),(24,233),(28,233),(28,220),(28,215),(28,207),(28,195),(24,195),(20,195),(16,195),(16,207),(16,215),(16,222),(16,233)]
-points_2 = [(300,22),(295,22),(290,22),(285,22),(285,27),(285,33),(285,40)]
-initial_direction = 90 # [N, E, S, W] = [0, 45, 180, 270]
+points_2 = [(300,22),(295,22),(290,22),(285,22),(285,27),(285,33),(285,40),(285,44),(275,44),(270,44),(265,44),(260,44)]
+initial_direction = 270 # [N, E, S, W] = [0, 45, 180, 270]
 
 parking_base = Image.open('media/parking_base.jpg') # width: 328, height: 260
 car = Image.open('media/car_resized.jpg')
 
 def initial_position(entrance):
     '''
-    Changes the angle of the car depending the initial position (if the car starts on entrance 2, the angle remains as the origial)
+    Changes the angle of the car depending the initial position (if the car starts on entrance 4, the angle remains as the origial)
     '''
     global parking_base, car
 
@@ -67,4 +67,4 @@ def main(coordinates, initial_direction):
 
     cv2.destroyAllWindows()
 
-main(points,  initial_direction)
+main(points_2,  initial_direction)
