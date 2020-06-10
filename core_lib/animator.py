5d3c12d5f0d140447f1514857ae4f5f58697dc54
Salvador Alexis Virgen Flores <salexisvirgen@gmail.com>
2020-06-10 05:03:29 -0500

    minor changes to algorithm

--- core_lib/animator.py
+++ core_lib/animator.py
@@ -10,8 +10,9 @@ from PIL import Image
 from core import get_config_value
 
 # this two variables will be provided by some other module
-points = [(8,233),(12,233),(16,233),(20,233),(24,233),(28,233),(28,220),(28,215),(28,207),(28,195),(24,195),(20,195),(16,195)]
-initial_direction = 90 # []
+points = [(8,233),(12,233),(16,233),(20,233),(24,233),(28,233),(28,220),(28,215),(28,207),(28,195),(24,195),(20,195),(16,195),(16,207),(16,215),(16,222),(16,233)]
+points_2 = [(300,22),(295,22),(290,22),(285,22),(285,27),(285,33),(285,40)]
+initial_direction = 90 # [N, E, S, W] = [0, 45, 180, 270]
 
 parking_base = Image.open('media/parking_base.jpg') # width: 328, height: 260
 car = Image.open('media/car_resized.jpg')
@@ -32,6 +33,9 @@ def initial_position(entrance):
         car = car.rotate(270, expand=1)
 
 def get_direction(origin, destination):
+    '''
+    Gets the angle between the two points
+    '''
     delta_x = destination[0] - origin[0]
     delta_y = destination[1] - origin[1]
 
@@ -44,17 +48,17 @@ def get_direction(origin, destination):
 
 def main(coordinates, initial_direction):
     global parking_base, car
-    initial_position(points[0])
+    initial_position(coordinates[0])
     
     current_direction = initial_direction
-    for i in range(len(points)):
+    for i in range(len(coordinates)):
         if i > 0:
-            degrees = get_direction(points[i-1], points[i])
-            car = car.rotate(degrees-current_direction, expand=1)
+            degrees = get_direction(coordinates[i-1], coordinates[i])
+            car = car.rotate(degrees-current_direction, expand=1) # rotates the car depending on the angle
             current_direction = degrees
 
         back_im = parking_base.copy()
-        back_im.paste(car, points[i])
+        back_im.paste(car, coordinates[i])
         opencv_image = cv2.cvtColor(numpy.array(back_im), cv2.COLOR_RGB2BGR)
 
         cv2.imshow('image_results', opencv_image)
@@ -64,4 +68,3 @@ def main(coordinates, initial_direction):
     cv2.destroyAllWindows()
 
 main(points,  initial_direction)
-print(get_direction(points[4], points[5]))
