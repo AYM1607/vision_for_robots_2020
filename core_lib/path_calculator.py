9c3581aa3b3936331f0680fa4cf5a1bd7b6c2aa5
unknown <beto.911599@gmail.com>
2020-06-10 04:49:17 -0500

    Implemented path calculator

--- core_lib/path_calculator.py
+++ core_lib/path_calculator.py
@@ -1,17 +1,20 @@
-import cv2
+import sys
+import csv
 import numpy as np
-from .parking import get_parking_poles_structure
-from .core import get_config_value
-from .parking import get_initial_coordinates_and_direction
+from parking import get_parking_poles_structure
+from core import get_config_value
+from parking import get_initial_coordinates_and_direction
+from core import Direction
 
 
-def get_neighbours(point, image, visited):
+def get_neighbours(point, image, visited, config):
     """
     Get the valid unvisited neighbours for a given point.
     Arguments:
         point {tuple} -- point representation, elements order: x, y.
         image {np.array} -- the original image.
         visited {np.array} -- visited matrix (booleans).
+        config {string | None} -- key for the virtual blocks given a coinfiguration.
     Returns:
         Array - includes all the valid unvisited neighbours, can be empty.
     """
@@ -27,11 +30,12 @@ def get_neighbours(point, image, visited):
         new_x = base_x + x_offset
         new_y = base_y + y_offset
 
-        if 0 <= new_x < width and 0 <= new_y < height:
-            if visited[new_y][new_x]:
+        if 0 <= new_x < width and 0 <= new_y < height and image[new_y][new_x] > -1:
+            if visited[new_y][new_x] or is_point_blocked((new_x, new_y), config):
                 continue
             valid_neighbours.append((new_x, new_y))
-            visited[new_y][new_x] = True
+            if config is None:
+                visited[new_y][new_x] = True
 
     return valid_neighbours
 
@@ -49,32 +53,125 @@ def calculate_distances(initial_point):
     """
     # Get downsampled image.
     poles = get_parking_poles_structure()
-
     # Create matrix of visited nodes.
     height = len(poles)
     width = len(poles[0])
     visited = np.full((height, width), False)
-
+    visited[initial_point[1]][initial_point[0]] = True
     # Enqueue initial point.
     queue = [initial_point]
     distance = 0
-    
     # BFS expansion, distances calculation.
     while len(queue) > 0:
-        aux_queue = queue
-        for curr_point in aux_queue:
-            queue.pop(0)
-            x, y = curr_point
-            if poles[y][x] > -1:
-                poles[y][x] = distance
-            queue.extend(get_neighbours(curr_point, poles, visited))
+        temp_size = len(queue)
+        for i in range(temp_size):
+            x, y = queue.pop(0)
+            poles[y][x] = distance
+            valid_neighbours = get_neighbours((x, y), poles, visited, None)
+            queue.extend(valid_neighbours)
         distance += 1
 
     return poles
+
+
+def is_point_blocked(point, config):
+    """
+   Defines if a point is inside a virtual block given a configuration => quadrant > direction.
+
+    Arguments:
+        point {tuple} -- Coordinates of a point (x, y).
+        config {string} -- String representing the configuration.
+
+    Returns:
+        boolean
+    """
+    if config is None:
+        return False
+    x, y = point
+    block_obj = get_config_value("barriers")[config]
+    return block_obj["corner_1"][0] <= x <= block_obj["corner_2"][0] and block_obj["corner_1"][1] <= y <= block_obj["corner_2"][1]
+
+def get_next_neighbour(curr_node, distances, config, visited):
+    """
+    Returns the valid neighbour that has the smalles distance.
+
+    Arguments:
+        curr_node {tuple} -- Coordinates of a point (x, y).
+        distances {np.array} -- Matrix that contains the distances.
+        config {string} -- String representing the configuration.
+        visited {np.array} -- Matrix of booleans that represent visited nodes.
+
+    Returns:
+        tuple
+    """
+    neighbours = get_neighbours(curr_node, distances, visited, config)
+    if len(neighbours) == 0:
+        return None
+    min_dist = sys.maxsize
+    next_node = (0, 0)
+    for x, y in neighbours:
+        if distances[y][x] < min_dist:
+            min_dist = distances[y][x]
+            next_node = (x, y)
+    return next_node
+
+
+def calculate_path(initial_point, distances, config, visited):
+    """
+    Returns the path from the initial point to the parking slot.
+
+    Arguments:
+        curr_node {tuple} -- Coordinates of a point (x, y).
+        distances {np.array} -- Matrix that contains the distances.
+        config {string} -- String representing the configuration.
+        visited {np.array} -- Matrix of booleans that represent visited nodes.
+
+    Returns:
+        array
+    """
+    path = [initial_point]
+    x_curr, y_curr = initial_point
+    # Mark the initial point as visited.
+    visited[y_curr][x_curr] = True
+    while distances[y_curr][x_curr] > 0:
+        next_node = get_next_neighbour((x_curr, y_curr), distances, config, visited)
+        if next_node is None:
+            print(f'There was an error calculating the path! :( len = {len(path)}')
+            return []
+        print(f'x = {next_node[0]}\ty = {next_node[1]}')
+        x_curr, y_curr = next_node
+        visited[y_curr][x_curr] = True
+        path.append(next_node)
+    return path
     
-def calculate_path():
-    intial_conditions = get_initial_coordinates_and_direction()
-    if intial_conditions:
-        x_initial, y_initial, direction = intial_conditions
-        quadrant = get_config_value("initial_quadrant")
-        
+
+def write_csv(mat, name):
+    with open(name, 'w', newline='') as file:
+        writer = csv.writer(file)
+        for i in range(len(mat)):
+            writer.writerow(mat[i])
+
+def init():
+    """
+    Gets the information necessary to calculate the path and invokes the function.
+    """
+    (x_initial, y_initial), direction = get_initial_coordinates_and_direction()
+    print(f'x_initial = {x_initial}\ty_initial = {y_initial}')
+    parking_coords = get_config_value("initial_coordinates")
+    print(f'parking_coords =  {parking_coords}')
+    # Build key to query for blocks coordinates
+    quadrant = get_config_value("initial_quadrant")
+    config = Direction(quadrant).name + ">" + Direction(direction).name
+    print(f'config = {config}')
+    # Build matrix with distances
+    distances = calculate_distances(parking_coords)
+    #write_csv(distances, "output.csv")
+    # Create matrix of visited nodes.
+    height = len(distances)
+    width = len(distances[0])
+    print(f'Downsampled Image Dimensions => height = {height}  twidth = {width}')
+    visited = np.full((height, width), False)
+    path = calculate_path((x_initial, y_initial), distances, config, visited)
+    print(f'pathLen = {len(path)}')
+    
+init()
--- core_lib/set_barriers.py
+++ core_lib/set_barriers.py
@@ -2,10 +2,14 @@
 
 import cv2
 from core import save_config_value
+from core import get_config_value
 from core import Direction
 
 barriers_counter = 0
+
 barriers_container = {}
+
+# Possible initial configurations.
 keys = [
     Direction.NORTH_WEST.name + ">" + Direction.SOUTH.name,
     Direction.NORTH_WEST.name + ">" + Direction.EAST.name,
@@ -17,11 +21,38 @@ keys = [
     Direction.SOUTH_EAST.name + ">" + Direction.WEST.name
 ]
 
+# Blocks are represented as a rectangle.
+# corner_1 refers to the top left corner.
+# corner_2 refers to the bottom right corner.
 sub_keys = [
     "corner_1",
     "corner_2"
 ]
 
+def get_scaling_factor():
+    """
+    Indicates by how much the parking poles structure was
+    scaled with respect to the original image.
+
+    Returns:
+        tuple --- the order is x_scale then y_scale
+    """
+
+    return get_config_value("x_scale"), get_config_value("y_scale")
+
+def scale_single_point(point):
+    x_scale, y_scale = get_scaling_factor()
+    x = int(round(point[0] * x_scale))
+    y = int(round(point[1] * y_scale))
+    return x, y
+
+
+def scale_barriers():
+    global barriers_container
+    for conf in barriers_container:
+        barriers_container[conf]["corner_1"] = scale_single_point(barriers_container[conf]["corner_1"])
+        barriers_container[conf]["corner_2"] = scale_single_point(barriers_container[conf]["corner_2"])
+        
 
 def mouse_callback(event, x, y, _flags, _params):
     global barriers_counter, barriers_container, keys, sub_keys
@@ -41,7 +72,7 @@ def set_barriers():
     cv2.setMouseCallback("parking", mouse_callback)
     print("Block " + keys[0] + "\t" + sub_keys[0])
 
-    while (int(barriers_counter / 2)) < len(keys):
+    while True:
         for block in barriers_container:
             if barriers_counter % 2 == 0:
                 cv2.rectangle(base_image, barriers_container[block]["corner_1"], barriers_container[block]["corner_2"], (0, 0, 255), 2)
@@ -51,9 +82,11 @@ def set_barriers():
             break
         
     cv2.imwrite("./media/barriers.jpg", base_image)
-    print("Done!")
+    print("Scaling barriers")
+    scale_barriers()
     print("Saving rectangles to the config file")
     save_config_value("barriers", barriers_container)
+    print("Done!!")
     cv2.destroyAllWindows()
 
 set_barriers()
